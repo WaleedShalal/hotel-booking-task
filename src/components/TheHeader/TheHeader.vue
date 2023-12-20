c0594c9c04b82fa4f73f5b164792410af3a4ed75
WaleedShalal <waleed.elbana2011@gmail.com>
2023-12-20 16:30:16 +0200

    adding header

--- src/components/TheHeader/TheHeader.vue
+++ src/components/TheHeader/TheHeader.vue
@@ -1,7 +1,62 @@
 <template>
   <header>
-    <h1>The Header</h1>
+    <div>
+      <figure>
+        <img :src="Logo" alt="logo" />
+      </figure>
+      <nav>
+        <ul class="nav__list">
+          <li v-for="{ name, path } in navs" :key="name">
+            <RouterLink :to="path">
+              {{ name }}
+            </RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
-<style scoped></style>
+<script setup>
+import { RouterLink } from 'vue-router'
+
+import Logo from '@/assets/logo.svg'
+const navs = [
+  { name: 'Home', path: '/' },
+  { name: 'Booked Hotels', path: '/booked-hotels' }
+]
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+}
+header > div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+header figure {
+  width: 5rem;
+  height: 5rem;
+  margin-block-end: 0;
+}
+header figure img {
+  width: 100%;
+  height: 100%;
+}
+
+header nav {
+  flex-grow: 1;
+}
+.nav__list {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  column-gap: 3rem;
+}
+.nav__list > li a {
+  text-decoration: none;
+  font-size: 3rem;
+}
+</style>
